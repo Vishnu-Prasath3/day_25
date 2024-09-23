683621c2d42bdd61ead037ea2518689258790779
vishnuprasath03 <eeevishnuprasath@gmail.com>
2024-09-23 09:13:56 +0530

    for vercwl modification

--- React-use-context/vite.config.js
+++ React-use-context/vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
 
-  build: {
-    rollupOptions: {
-      // Ensure that the correct entry point is specified
-      input: './src/main.jsx', // Adjust this if your entry file is different
-      external: ['react', 'react-dom']
-    
-    }
-  }
 })
